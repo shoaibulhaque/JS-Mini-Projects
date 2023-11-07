41b76b5eeaad6b17f2c66521539c26dccc0fc33f
Shoaib <haq4402067@cloud.neduet.edu.pk>
2023-11-08 00:50:29 +0500

    Added json

--- chrome-ext/index.js
+++ chrome-ext/index.js
@@ -1,10 +1,37 @@
 // Variables
 let myLeads = [];
-const inputEl = document.querySelector("#input-el")
+
 
 // Fetching ids
-
+const inputEl = document.querySelector("#input-el")
+const ulEl = document.querySelector("#ul-el");
 const inputBtn = document.querySelector("#input-btn");
-inputBtn.addEventListener("click", () => {
 
+// Input btn
+inputBtn.addEventListener("click", () => {
+  myLeads.push(inputEl.value);
+
+  // Clear the value
+  inputEl.value = "";
+
+  // Call the render load function
+  renderLoad();
 });
+
+
+function renderLoad () {
+  // Looping through array
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+    <li>
+        <a target = '_blank' href = "${myLeads[i]}">
+        ${myLeads[i]}
+        </a>
+    </li>
+    `
+  }
+
+  // Append li to ul 
+  ulEl.innerHTML = listItems;
+}
