cc2b56210542a6bf543f59f33b1b6dc43a213f1c
Shoaib <haq4402067@cloud.neduet.edu.pk>
2023-11-07 23:30:05 +0500

    Setup env for project

---
+++ chrome-ext/index.js
@@ -0,0 +1,10 @@
+// Variables
+let myLeads = [];
+const inputEl = document.querySelector("#input-el")
+
+// Fetching ids
+
+const inputBtn = document.querySelector("#input-btn");
+inputBtn.addEventListener("click", () => {
+
+});
